f9647229d9a873aa0a6e2d4360c4d0762f8671be
Paweł Dybiec <pawel.to.malpa@gmail.com>
2019-11-08 16:55:55 +0100

    Almost solution

---
+++ level-4/solution.py
@@ -0,0 +1,70 @@
+from fractions import Fractional as F
+rows,columns = list(map(int,input().split()))
+n= int(input())
+def straight(o,d):
+    (ox,oy)=o
+    (dx,dy)=d
+    result = []
+    horizontal = 1 if dx>0 else -1
+    vertical = 1 if dy>0 else -1
+    if dx==0: horizontal=0
+    if dy==0: vertical=0
+    while 0<=ox<cols and 0<=oy<rows:
+        result.append((ox,oy))
+        ox+=horizontal
+        oy+=vertical
+    return result
+    
+def one_test(o,d):
+    (ox,oy)=o
+    (dx,dy)=d
+    horizontal = 1 if dx>0 else -1
+    vertical = 1 if dy>0 else -1
+    next_vertical = F(1,2)*horizontal+ox
+    next_horizontal = F(1,2)*vertical + oy
+    if dx==0 or dy==0:
+        return straight(o,d)
+    dominant_horizontal= math.abs(dx)>= math.abs(dy)
+
+    x=F(ox)
+    y=F(oy)
+    solution=[]
+    while 0<=ox<cols and 0<=oy<rows:
+        time1= (next_horizontal-y)/dy
+        time2 = (next_vertical-x)/dx
+        time=min(time1,time2)
+        if time1==time2:
+            solution.append((ox,oy))
+            if dominant_horizontal:
+                solution.append((ox+horizontal,oy))
+                solution.append((ox,oy+vertical))
+            else:
+                solution.append((ox,oy+vertical))
+                solution.append((ox+horizontal,oy))
+            x+=dx*time
+            y+=dy*time
+            ox+=horizontal
+            oy+=vertical
+            next_vertical+=vertical
+            next_horizontal+=horizontal
+        else:
+            solution.append((ox,oy))
+            x+=dx*time
+            y+=dy*time
+            if time1<time2:
+                next_vertical+=vertical
+                oy+=vertical
+            else:
+                next_horizontal+= horizontal
+                ox += horizontal
+    
+    return solution            
+        
+
+
+
+
+     
+for i in range(n):
+    ox,oy,dx,dy=list(map(int,input().split()))
+    one_test((ox,oy),(dx,dy))
